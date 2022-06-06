f88bf334b187a72749cda49ebde0b0340a8973be
Menyin Chang <menyin_chang@hotmail.com>
2022-06-06 17:37:08 -0300

    Add: creates HeaderNew constants

---
+++ src/components/HeaderNew/constants.js
@@ -0,0 +1,16 @@
+import { GitHub, Twitter, LinkedIn } from '@mui/icons-material';
+
+export const languages = [{ value: 'ES' }, { value: 'EN' }, { value: 'PT' }];
+
+export const socialNetworks = [
+	{ title: 'Github', Icon: () => <GitHub /> },
+	{ title: 'Twitter', Icon: () => <Twitter /> },
+	{ title: 'LinkedIn', Icon: () => <LinkedIn /> },
+];
+
+export const menuItems = [
+	{ title: 'Products', url: '', variant: null, visible: false },
+	{ title: 'Clients', url: '', variant: null, visible: false },
+	{ title: 'About', url: '', variant: null, visible: true },
+	{ title: 'Contact Us', url: '', variant: 'primary', visible: true },
+];
