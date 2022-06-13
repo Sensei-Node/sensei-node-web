b1d79ca5ed9496fd155c72671fea7bc774f6672a
Menyin Chang <menyin_chang@hotmail.com>
2022-06-13 09:45:22 -0300

    Update format file

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,21 +1,27 @@
-import React, { createContext, useContext, useState, useCallback, useMemo } from 'react';
+import React, {
+	createContext,
+	useContext,
+	useState,
+	useCallback,
+	useMemo,
+} from 'react';
 
 const ThemeContext = createContext();
 
 export const useThemeContext = () => useContext(ThemeContext);
 
 export default ({ children }) => {
-  const [theme, setTheme] = useState('light');
+	const [theme, setTheme] = useState('light');
 
-  const isLight = useMemo(() => theme === 'light', [theme]);
+	const isLight = useMemo(() => theme === 'light', [theme]);
 
-  const toggleTheme = useCallback(() => {
-    setTheme(prev => (prev === 'light' ? 'dark' : 'light'));
-  }, []);
+	const toggleTheme = useCallback(() => {
+		setTheme((prev) => (prev === 'light' ? 'dark' : 'light'));
+	}, []);
 
-  return (
-    <ThemeContext.Provider value={{ theme, isLight, toggleTheme }}>
-      {children}
-    </ThemeContext.Provider>
-  );
+	return (
+		<ThemeContext.Provider value={{ theme, isLight, toggleTheme }}>
+			{children}
+		</ThemeContext.Provider>
+	);
 };
