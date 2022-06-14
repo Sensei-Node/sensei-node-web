6333600f5c140a408ee9057ef414630d3c7f4157
Menyin Chang <menyin_chang@hotmail.com>
2022-06-14 09:30:33 -0300

    Add: .env file and Dotenv package to handle env variables

--- webpack.config.js
+++ webpack.config.js
@@ -3,120 +3,125 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 
 const prod = process.env.NODE_ENV === 'production';
 
 const loaders = {
-  html: {
-    loader: 'html-loader',
-  },
-  babel: {
-    loader: 'babel-loader',
-    options: {
-      compact: false,
-      cacheDirectory: !prod,
-    },
-  },
-  style: [
-    prod ? MiniCssExtractPlugin.loader : 'style-loader',
-    'css-loader',
-    'postcss-loader',
-    'sass-loader',
-  ],
-  url: {
-    loader: 'url-loader',
-    options: { limit: 8192 },
-  },
-  svg: {
-    loader: '@svgr/webpack',
-  },
+	html: {
+		loader: 'html-loader',
+	},
+	babel: {
+		loader: 'babel-loader',
+		options: {
+			compact: false,
+			cacheDirectory: !prod,
+		},
+	},
+	style: [
+		prod ? MiniCssExtractPlugin.loader : 'style-loader',
+		'css-loader',
+		'postcss-loader',
+		'sass-loader',
+	],
+	url: {
+		loader: 'url-loader',
+		options: { limit: 8192 },
+	},
+	svg: {
+		loader: '@svgr/webpack',
+	},
 };
 
 module.exports = {
-  mode: prod ? 'production' : 'development',
-  devtool: prod ? false : 'eval-source-map',
-  entry: {
-    app: resolve(__dirname, 'src/index'),
-  },
-  devServer: {
-    contentBase: resolve(__dirname, './emailsignature'),
-    contentBasePublicPath: '/emailsignature'
-  },
-  resolve: {
-    modules: ['node_modules'],
-    mainFiles: ['index'],
-    extensions: ['.js', '.mjs', '.jsx', '.json'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.html$/,
-        use: [loaders.html],
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: [loaders.babel],
-      },
-      {
-        test: /\.(c|sc|sa)ss$/,
-        use: loaders.style,
-      },
-      {
-        test: /\.(jpe?g|png|gif|bmp)$/,
-        use: [loaders.url],
-      },
-      {
-        test: /\.svg$/,
-        use: [loaders.svg, loaders.url],
-      },
-      {
-        test: /\.mp4$/,
-        use: [
-            {
-                loader: "file-loader",
-                options: {
-                    name: "[name].[ext]",
-                    outputPath: "video"
-                }
-            }
-        ]
-      }
-    ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      hash: true,
-      template: 'public/index.html',
-      favicon: 'public/favicon.ico',
-      minify: {
-        collapseWhitespace: true,
-      },
-    }),
-    new CleanWebpackPlugin(),
-    ...(prod
-      ? [
-          new MiniCssExtractPlugin({
-            filename: 'static/css/[name].[contenthash:8].css',
-            chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
-          }),
-        ]
-      : []),
-  ],
-  optimization: {
-    minimize: prod,
-    minimizer: [
-      new TerserWebpackPlugin({
-        parallel: true,
-        terserOptions: {
-          safari10: true,
-        },
-      }),
-    ],
-  },
-  output: {
-    path: resolve(__dirname, 'dist'),
-    filename: 'static/js/[name].[contenthash:8].js',
-    chunkFilename: 'static/js/[name].[id].[contenthash:8].chunk.js',
-  },
+	mode: prod ? 'production' : 'development',
+	devtool: prod ? false : 'eval-source-map',
+	entry: {
+		app: resolve(__dirname, 'src/index'),
+	},
+	devServer: {
+		contentBase: resolve(__dirname, './emailsignature'),
+		contentBasePublicPath: '/emailsignature',
+	},
+	resolve: {
+		modules: ['node_modules'],
+		mainFiles: ['index'],
+		extensions: ['.js', '.mjs', '.jsx', '.json'],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.html$/,
+				use: [loaders.html],
+			},
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				use: [loaders.babel],
+			},
+			{
+				test: /\.(c|sc|sa)ss$/,
+				use: loaders.style,
+			},
+			{
+				test: /\.(jpe?g|png|gif|bmp)$/,
+				use: [loaders.url],
+			},
+			{
+				test: /\.svg$/,
+				use: [loaders.svg, loaders.url],
+			},
+			{
+				test: /\.mp4$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'video',
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			hash: true,
+			template: 'public/index.html',
+			favicon: 'public/favicon.ico',
+			minify: {
+				collapseWhitespace: true,
+			},
+		}),
+		new CleanWebpackPlugin(),
+		...(prod
+			? [
+					new MiniCssExtractPlugin({
+						filename: 'static/css/[name].[contenthash:8].css',
+						chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
+					}),
+			  ]
+			: []),
+		new Dotenv({
+			path: './.env',
+			safe: true,
+		}),
+	],
+	optimization: {
+		minimize: prod,
+		minimizer: [
+			new TerserWebpackPlugin({
+				parallel: true,
+				terserOptions: {
+					safari10: true,
+				},
+			}),
+		],
+	},
+	output: {
+		path: resolve(__dirname, 'dist'),
+		filename: 'static/js/[name].[contenthash:8].js',
+		chunkFilename: 'static/js/[name].[id].[contenthash:8].chunk.js',
+	},
 };
